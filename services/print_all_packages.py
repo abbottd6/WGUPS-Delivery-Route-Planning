c1d95113b0fff104151af1fcf3c855d796102dc4
Dayton <dayton.abbott@yahoo.com>
2025-05-08 18:39:49 -0700

    cleaning up print formatting for readability, switching getter call to get_by_id.

--- services/print_all_packages.py
+++ services/print_all_packages.py
@@ -5,9 +5,10 @@ from services.package_data_parser import package_keys
 line_format = ""
 
 def print_all_packages():
-    print("\n Currently Tracked Packages: \n")
+    print(f"{line_format:_<200}\n")
+    print("Currently Tracked Packages:")
     print(f"{line_format:_<200}\n")
     for package_id in package_keys:
-        print(package_hash_table.get(package_id))
+        print(package_hash_table.get_by_id(package_id))
         time.sleep(0.025)
     print(f"\n{line_format:_<200}\n")
--- test/test_prints.py
+++ test/test_prints.py
@@ -12,7 +12,7 @@ if (len(package_keys) == 40) and (package_hash_table.count == 40):
     # test print to check hash table creation and population from csv file
     print("\n \n Packages successfully transferred to hash table: \n")
     for package_id in package_keys:
-        print(package_hash_table.get(package_id))
+        print(package_hash_table.get_by_id(package_id))
 
 # testing append_note method for adding note about duplicate id's if key already exists
 # during insert into hash table
@@ -20,4 +20,4 @@ test_package = package.Package(38, '410 S State St', 'Salt Lake City', 'UT',
                                '84111', 'EOD', '9', 'Can only be on truck 2')
 package_hash_table.insert(test_package.package_id, test_package)
 
-print(package_hash_table.get(test_package.package_id))
+print(package_hash_table.get_by_id(test_package.package_id))
