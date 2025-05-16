08b6ca5227572b3046f90ee06810fe901afdb111
Dayton <dayton.abbott@yahoo.com>
2025-05-15 20:15:20 -0700

    Add prioritization in nearest_neighbor_path_generator for packages where deadline is less than other deadlines.

--- services/nearest_neighbor_path_generator.py
+++ services/nearest_neighbor_path_generator.py
@@ -1,5 +1,7 @@
 from utils.calc_travel_time import calc_travel_time
+from datetime import datetime
 
+time_format = "%I:%M %p"
 line_format = ''
 
 def nearest_neighbor_path_generator(some_package_keys, some_package_hash_table,
@@ -9,10 +11,32 @@ def nearest_neighbor_path_generator(some_package_keys, some_package_hash_table,
     current_node_address = ''
     current_node_index = 0
     visited_nodes = []
+    high_priority_packages = []
+    high_priority_package_keys = []
     aggregate_time = 0.0
     destination_count = 0
     num_packages_delivered = 0
 
+    # Determine whether there are packages with deadline priority
+    deadline_package_keys = []
+    for package_key in some_package_keys:
+        if "EOD" not in some_package_hash_table.get_by_id(package_key).deadline:
+            deadline_package_keys.append(package_key)
+
+    for package_key in deadline_package_keys:
+        package_to_check = some_package_hash_table.get_by_id(package_key)
+        if "EOD" in package_to_check.deadline:
+            continue
+        this_deadline = datetime.strptime(package_to_check.deadline, time_format).time()
+        if any(
+                this_deadline < (datetime.strptime(some_package_hash_table.get_by_id(other_key).deadline,
+                    time_format).time())
+                for other_key in deadline_package_keys
+        ):
+            high_priority_packages.append(package_to_check)
+            high_priority_package_keys.append(package_key)
+            for package in high_priority_packages:
+                print("high priority packages", package)
 
     # Find the starting node i.e., the hub.
     for i, row in enumerate(some_distance_matrix[1:], start=1):
@@ -22,63 +46,77 @@ def nearest_neighbor_path_generator(some_package_keys, some_package_hash_table,
             current_node_index = i
         destination_count = i
 
-    # Determine whether there are packages with deadline priority
-    if any(package.deadline in package for package in some_package_hash_table) < any(package.deadline in package for package in some_package_hash_table):
-        print("This is going to loop forever if true")
+    if high_priority_packages:
+        for package_key in high_priority_package_keys:
+            this_address = some_package_hash_table.get_by_id(package_key).address
+            associated_packages = some_package_hash_table.get_by_address(
+                some_package_hash_table.get_by_id(package_key).address)
+            visited_nodes.append(associated_packages[0].address)
+
+            for i, address in enumerate(some_distance_matrix[0][1:], start=1):
+                if this_address in address:
+                    this_address_index = i
+                    break
+            distance_traveled_array.append(some_distance_matrix[this_address_index][1])
+            current_node_index = this_address_index
+            current_node_address = this_address
+
+            num_packages_delivered += len(associated_packages)
+        high_priority_packages.clear()
 
 
-    # Create an empty dictionary to hold the distances TO other destinations FROM current_node.
-    neighbor_distances_array = {}
-    for delivery_stop in range(1, destination_count + 1):
+    if not high_priority_packages:
+        # Create an empty dictionary to hold the distances TO other destinations FROM current_node.
+        neighbor_distances_array = {}
+        for delivery_stop in range(len(high_priority_package_keys), destination_count + 1):
+            # Add each neighbor distance for current_node to the distance dictionary.
+            for row in some_distance_matrix[1:]:
+                # First, check that the distance being added does not correspond to an already visited
+                # destination.
+                if not any(row[0] in node for node in visited_nodes):
+                    # If the distance value does not correspond to a visited node, add it to distance
+                    # dictionary, where the key is the node address and the value is the distance to
+                    # that node from current_node.
+                    neighbor_distances_array[row[0]] = row[current_node_index]
+                else:
+                    continue
 
-        # Add each neighbor distance for current_node to the distance dictionary.
-        for row in some_distance_matrix[1:]:
-            # First, check that the distance being added does not correspond to an already visited
-            # destination.
-            if not any(row[0] in node for node in visited_nodes):
-                # If the distance value does not correspond to a visited node, add it to distance
-                # dictionary, where the key is the node address and the value is the distance to
-                # that node from current_node.
-                neighbor_distances_array[row[0]] = row[current_node_index]
-            else:
-                continue
+            # Identify the nearest neighbor to be the dictionary element containing the smallest
+            # distance value.
+            nearest_neighbor = min(neighbor_distances_array.items(), key=lambda item: item[1])
 
-        # Identify the nearest neighbor to be the dictionary element containing the smallest
-        # distance value.
-        nearest_neighbor = min(neighbor_distances_array.items(), key=lambda item: item[1])
+            # Adjust current_node_index to be this nearest_neighbor in prep for next iteration.
+            for i, row in enumerate(some_distance_matrix[1:], start=1):
+                if row[0] == nearest_neighbor[0]:
+                    current_node_index = i
 
-        # Adjust current_node_index to be this nearest_neighbor in prep for next iteration.
-        for i, row in enumerate(some_distance_matrix[1:], start=1):
-            if row[0] == nearest_neighbor[0]:
-                current_node_index = i
+            # print("NEXT PATH NODE AND DISTANCE:", nearest_neighbor)
+            # print("NEXT_NODE 'DISTANCE MATRIX' ROW INDEX:", current_node_index)
 
-        # print("NEXT PATH NODE AND DISTANCE:", nearest_neighbor)
-        # print("NEXT_NODE 'DISTANCE MATRIX' ROW INDEX:", current_node_index)
+            # Gather packages associated with the address of nearest_neighbor into a hash table.
+            associated_packages = some_package_hash_table.get_by_address(nearest_neighbor[0])
 
-        # Gather packages associated with the address of nearest_neighbor into a hash table.
-        associated_packages = some_package_hash_table.get_by_address(nearest_neighbor[0])
+            # Add associated_packages address to visited nodes if there are packages associated with
+            # this address.
+            visited_nodes.append(associated_packages[0].address)
+            # print(visited_nodes[0])
 
-        # Add associated_packages address to visited nodes if there are packages associated with
-        # this address.
-        visited_nodes.append(associated_packages[0].address)
-        # print(visited_nodes[0])
+            path_time = calc_travel_time(nearest_neighbor[1])
 
-        path_time = calc_travel_time(nearest_neighbor[1])
+            aggregate_time += path_time
+            distance_traveled_array.append(nearest_neighbor[1])
+            neighbor_distances_array[current_node_address] = nearest_neighbor[0]
 
-        aggregate_time += path_time
-        distance_traveled_array.append(nearest_neighbor[1])
-        neighbor_distances_array[current_node_address] = nearest_neighbor[0]
+            # Empty reusable data structure for next iteration.
+            neighbor_distances_array.clear()
 
-        # Empty reusable data structure for next iteration.
-        neighbor_distances_array.clear()
+            # Aggregate the total number of packages delivered in this route for
+            # termination upon delivery of all packages.
+            num_packages_delivered += len(associated_packages)
 
-        # Aggregate the total number of packages delivered in this route for
-        # termination upon delivery of all packages.
-        num_packages_delivered += len(associated_packages)
-
-        # Terminate route planning if all packages have been accounted for.
-        if num_packages_delivered >= len(some_package_keys):
-            break
+            # Terminate route planning if all packages have been accounted for.
+            if num_packages_delivered >= len(some_package_keys):
+                break
 
     # Create a dictionary to insert optimized route based on delivery sequence value (as key).
     nearest_neighbor_route = {}
@@ -113,4 +151,5 @@ def nearest_neighbor_path_generator(some_package_keys, some_package_hash_table,
             nearest_neighbor_route[d + 1] = route_termination_info
 
     # Return the route dictionary for assignment where called.
+    print("This is the end of one path generator")
     return nearest_neighbor_route
